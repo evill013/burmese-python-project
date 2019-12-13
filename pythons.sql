e971d776d253026c68415c8603379bdceae47edb
evill013 <mvillavicencio@Marias-MacBook-Pro-2.local>
2019-12-12 19:48:37 -0500

    uploaded_files

--- pythons.sql
+++ pythons.sql
@@ -77,7 +77,7 @@ CREATE TABLE `pythons` (
   `Museum` text,
   `MuseumRec` text,
   `Reference` text
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
